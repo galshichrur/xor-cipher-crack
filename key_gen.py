eaef8058559fe4177b70ecead800ead38b32ae0c
Gal <82903124+maybegal@users.noreply.github.com>
2025-09-20 19:59:40 +0300

    Update to import from config.py file

--- key_gen.py
+++ key_gen.py
@@ -1,18 +1,17 @@
 from collections.abc import Iterator
+from config import Config
 
-ALLOWED_KEY_CHARS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-                 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def generate_all_keys(key_size: int) -> Iterator[str]:
     """
     Generator of all possible key combinations using the given key size and ALLOWED_KEY_CHARS.
     """
 
-    base = len(ALLOWED_KEY_CHARS)
+    base = len(Config.ALLOWED_KEY_CHARS)
     index_list = [0] * key_size
 
     while True:
-        key = "".join(ALLOWED_KEY_CHARS[i] for i in index_list)
+        key = "".join(Config.ALLOWED_KEY_CHARS[i] for i in index_list)
         yield key
 
         pos = key_size - 1
